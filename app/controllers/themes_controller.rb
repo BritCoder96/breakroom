096a17126afcd0775a15628264f4fd3974d13472
Bryan Benson <bryanbenson83@gmail.com>
2020-06-11 16:47:53 -0400

    Change topic/theme schema; add organizations to topcs; fix error handling for theme creation

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -2,7 +2,7 @@ class ThemesController < ApplicationController
     before_action :authenticate_user!
 
     def index
-        @themes = Theme.all
+        @themes = Theme.where(organization_id: [current_user.organization_id, nil]).includes(:topics).load
     end
 
     def new
@@ -11,7 +11,7 @@ class ThemesController < ApplicationController
 
     def show
         @theme = Theme.find_by(id: params[:id])
-        @themes = Theme.all
+        @themes = Theme.where(organization_id: [current_user.organization_id, nil]).includes(:topics).load
     end
 
     def create
@@ -47,6 +47,6 @@ class ThemesController < ApplicationController
     private
 
     def theme_params
-        params.require(:theme).permit(:name)
+        params.require(:theme).permit(:name, :organization_id)
     end
 end
---
+++ db/migrate/20200611193938_add_organization_to_themes.rb
@@ -0,0 +1,5 @@
+class AddOrganizationToThemes < ActiveRecord::Migration[6.0]
+    def change
+        add_reference :themes, :organization, foreign_key: true
+    end
+end
