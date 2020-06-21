c1778108daefff293b6c29897a6f77c443419eb6
Bryan Benson <bryanbenson83@gmail.com>
2020-06-21 12:38:26 -0400

    Fix ajax scroll and filter; fix bit button box

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function() {
     if ($('#infinite-scrolling').size() > 0) {
-        $(window).on('scroll', function() {
+        $(document).on('scroll', function() {
             var more_conversations_url;
             more_conversations_url = "/?conversation_page=" + ($('#conversations-feed').data('page') + 1);
             if (more_conversations_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
